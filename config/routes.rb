9f1b570f4e920e6e6245c10891491d714a482d41
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2023-02-17 23:54:03 +0100

    errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server'
+  get '/422', to: 'errors#unprocessable'
+  get '/406', to: 'errors#unacceptable'
   get '/:book_slug', to: 'books#show', as: :book
   get '/:book_slug/:chapter_slug', to: 'chapters#show', as: :chapter
 
